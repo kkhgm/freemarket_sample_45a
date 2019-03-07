95aef6d8e220e3f718a575b045261862b74d3644
ymtwithcarp <kkcymt@yahoo.co.jp>
2019-03-07 18:04:35 +0900

    update 商品出品ページ　画面表示

--- app/assets/javascripts/item_sales.js
+++ app/assets/javascripts/item_sales.js
@@ -1,21 +1,12 @@
 $(function(){
-	var html =
-	`<div class="contents__shipping_box_method">
-		<h4>配送の方法<span>必須</span></h4>
-		<select class="contents__shipping_box_method_input">
-			<option>---</option>
-			<option>未定</option>
-			<option>クロネコヤマト</option>
-			<option>ゆうパック</option>
-			<option>ゆうメール</option>
-		</select>
-	<div>`;
 
 	$("#item_shipping_charge").on("change", function(){
-		$(".contents__shipping_box_method").remove();
+		// $(".contents__shipping_box_method").remove();
 		var select = $("#item_shipping_charge").val();
 		if(select == 1 || select == 2){
-		$(".contents__shipping_box_sub").append(html)
+		$(".contents__shipping_box_method").show()
+		}else{
+			$(".contents__shipping_box_method").hide();
 		}
 	})
 })
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,16 +7,19 @@ class ItemsController < ApplicationController
 
   def new
   	@item = Item.new
+    # @item.build_item_images
   	@regions = Region.all
   end
 
   def create
   	@item = Item.create(item_params)
+    binding.pry
+    redirect_to controller: :items, action: :index
   end
 
   private
   def item_params
-  	params.permit(:name,:description,:shipping_method,:shipping_change,:ship_from_region,:shipping_date,:price)
+  	params.require(:item).permit(:name,:description,:condition,:shipping_method,:shipping_charge,:ship_from_region,:shipping_date,:price)
   end
 
   def show

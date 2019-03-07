a996225cacb95dcba506e87f451ed7ce701b305a
cb50one <nakajimatakuyume@cb50one.local>
2019-03-07 17:00:14 +0900

    [add] user validates
    ユーザー情報にバリデーションをかけました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one :address
+  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday_info, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end

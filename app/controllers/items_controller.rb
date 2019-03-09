e286a96c565419f902a21568135a34a75241f10b
kkhgm <koki@higamimitsuterunoMacBook-Air.local>
2019-03-09 13:59:21 +0900

    Add updateアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-
   end
 
   def edit
     @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
   end
 
 end

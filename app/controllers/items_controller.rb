32f6a5d311df58f26f4223fbcd7af48f72ea461a
kkhgm <koki@higamimitsuterunoMacBook-Air.local>
2019-03-09 16:55:18 +0900

    Add before_actionの設定
    定義したメソッドをedit, update, destroy アクション実行前に
    メソッドが実行されるよう変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :each_item, except:[:index, :show]
+
   def index
     @items = Item.order("created_at DESC").limit(4)
   end
@@ -7,16 +9,13 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    each_item
   end
 
   def update
-    each_item
     render :action => "edit" unless @item.update(item_params)
   end
 
    def destroy
-    each_item
     if @item.destroy
       redirect_to action: "index"
     else

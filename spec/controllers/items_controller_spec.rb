761085acb727fe24aca085631ed20f403676e9b2
kkhgm <koki@higamimitsuterunoMacBook-Air.local>
2019-03-09 16:06:37 +0900

    Add テストの追加

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -12,6 +12,14 @@ describe ItemsController, type: :controller do
       get :index
       expect(response).to render_template :index
     end
+  end
 
+  describe 'delete #destroy' do
+    it "削除機能の確認" do
+      items = create(:item)
+      expect{
+        delete :destroy, params: { id: items}
+      }. to change(Item, :count).by(-1)
+    end
   end
 end

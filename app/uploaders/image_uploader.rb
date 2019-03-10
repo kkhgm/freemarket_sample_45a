151844ffb4fd4f9cf99c3cae99dbde7a83baa94c
ymtwithcarp <kkcymt@yahoo.co.jp>
2019-03-10 11:59:37 +0900

    update image_uploader.rbの記述の変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,9 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
+  if Rails.env.development? || Rails.env.test?
     storage :file
   else
     storage :fog

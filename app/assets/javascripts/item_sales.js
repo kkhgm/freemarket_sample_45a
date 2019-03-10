bbacd5549b538edf6b417376affd7695249ca8b3
ymtwithcarp <kkcymt@yahoo.co.jp>
2019-03-10 14:19:25 +0900

    update js,scssファイルのインデント変更

--- app/assets/javascripts/item_sales.js
+++ app/assets/javascripts/item_sales.js
@@ -1,11 +1,10 @@
 $(function(){
-
-	$("#item_shipping_charge").on("change", function(){
-		var select = $("#item_shipping_charge").val();
-		if(select == 1 || select == 2){
-		$(".item-contents__shipping_box_method").show()
-		}else{
-			$(".item-contents__shipping_box_method").hide();
-		}
-	})
+  $("#item_shipping_charge").on("change", function(){
+    var select = $("#item_shipping_charge").val();
+    if(select == 1 || select == 2){
+    $(".item-contents__shipping_box_method").show()
+    }else{
+      $(".item-contents__shipping_box_method").hide();
+    }
+  })
 })
--- app/assets/javascripts/price_calulate.js
+++ app/assets/javascripts/price_calulate.js
@@ -1,16 +1,16 @@
 $(function() {
-	$(".contents__price_box_list_top_right_input").on('keyup', function(e){
-		e.preventDefault();
-		var count = $(this).val();
-		if(count >= 300){
-			var cut = count * 0.1
-			var cutMoney = Math.floor(cut);
-			$(".contents__price_box_list_sales").text("¥" + cutMoney);
-			var salesMoney = count - cutMoney
-			$(".contents__price_box_list_sales_under").text("¥" + salesMoney);
-		} else {
-			$(".contents__price_box_list_sales").text("-");
-			$(".contents__price_box_list_sales_under").text("-");
-		}
-	});
+  $(".contents__price_box_list_top_right_input").on('keyup', function(e){
+    e.preventDefault();
+    var count = $(this).val();
+    if(count >= 300){
+      var cut = count * 0.1
+	  var cutMoney = Math.floor(cut);
+	  $(".contents__price_box_list_sales").text("¥" + cutMoney);
+	  var salesMoney = count - cutMoney
+	  $(".contents__price_box_list_sales_under").text("¥" + salesMoney);
+    } else {
+      $(".contents__price_box_list_sales").text("-");
+	  $(".contents__price_box_list_sales_under").text("-");
+    }
+  });
 });

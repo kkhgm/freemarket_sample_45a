ee1fd1c1b5153ec2622c3c6ab2af59eb64742d47
ymtwithcarp <kkcymt@yahoo.co.jp>
2019-03-10 16:25:04 +0900

    update registrations_controller不要部分の削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  layout "users"
   # GET /resource/sign_up
   def new0
   end

18e687cccc701e11e57c6d9cf8370a8e7bb0f48a
ymtwithcarp <kkcymt@yahoo.co.jp>
2019-03-10 11:58:31 +0900

    update 改行の追加

--- app/assets/javascripts/item_sales.js
+++ app/assets/javascripts/item_sales.js
@@ -1,12 +1,11 @@
 $(function(){
 
 	$("#item_shipping_charge").on("change", function(){
-		// $(".contents__shipping_box_method").remove();
 		var select = $("#item_shipping_charge").val();
 		if(select == 1 || select == 2){
-		$(".contents__shipping_box_method").show()
+		$(".item-contents__shipping_box_method").show()
 		}else{
-			$(".contents__shipping_box_method").hide();
+			$(".item-contents__shipping_box_method").hide();
 		}
 	})
 })

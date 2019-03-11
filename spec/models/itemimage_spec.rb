6c0125c7297fbe626b57a42a1011a67fc4508fed
ymtwithcarp <kkcymt@yahoo.co.jp>
2019-03-11 15:05:20 +0900

    update itemimage.specインデントの変更

--- spec/models/itemimage_spec.rb
+++ spec/models/itemimage_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Itemimage, type: :model do
 	describe Itemimage do
-	  it "画像が保存できていないとバリデーションで弾かれる" do
-	    itemimage = build(:itemimage, image: nil)
-	    itemimage.valid?
-	    expect(itemimage.errors[:image]).to include("can't be blank")
-	  end
-	end
+　　it "画像が保存できていないとバリデーションで弾かれる" do
+　　　itemimage = build(:itemimage, image: nil)
+　　　itemimage.valid?
+　　　expect(itemimage.errors[:image]).to include("can't be blank")
+　　end
+　end
 end

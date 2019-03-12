df72dbd4d16ac5ebb49e0e8f59bb85fe4d27942e
ymtwithcarp <kkcymt@yahoo.co.jp>
2019-03-12 20:54:34 +0900

    update models/itemimage_spec変数名の変更

--- spec/models/itemimage_spec.rb
+++ spec/models/itemimage_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe Itemimage, type: :model do
 	describe Itemimage do
     it "画像が保存できていないとバリデーションで弾かれる" do
-      itemimage = build(:itemimage, image: nil)
-      itemimage.valid?
-      expect(itemimage.errors[:image]).to include("can't be blank")
+      item_image = build(:itemimage, image: nil)
+      item_image.valid?
+      expect(item_image.errors[:image]).to include("can't be blank")
     end
   end
 end

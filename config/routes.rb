3696a13861528a8c40c375dc73b5d4178b09f934
cb50one <nakajimatakuyume@cb50one.local>
2019-03-16 19:59:09 +0900

    Add sns新規登録のview

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   omniauth_callbacks: 'users/omniauth_callbacks'
   }
   devise_scope :user do
+    get 'signinAgain' => 'users/sessions#newAgain', as: :newAgain_user_session
     get 'signup0' => 'users/registrations#new0', as: :new0_user_registration
+    get 'signupsns' => 'users/registrations#sns', as: :new_user_registration_sns
     # get 'signup1' => 'devise/registrations#new1', as: :new1_user_registration
     get 'signup2' => 'users/registrations#new2', as: :new2_user_registration
     get 'signup25' => 'users/registrations#new25', as: :new25_user_registration

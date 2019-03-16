a73103e588bc72a0083f2972783dc61ed7a15d8f
cb50one <nakajimatakuyume@cb50one.local>
2019-03-16 20:02:31 +0900

    Add dependent: :destroy
    ユーザーが削除されたらsns_credentialsも削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one :address
-  has_many :sns_credentials
+  has_many :sns_credentials, dependent: :destroy
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday_info, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
@@ -11,14 +11,19 @@ class User < ApplicationRecord
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first
     unless sns
       @user = User.new(
+        nickname: auth.info.name,
         email: auth.info.email,
         password: Devise.friendly_token[0,20]
         )
-      # @user.save(validate: false)
-      ssns = SnsCredential.new(
+      @user.save(validate: false)
+      @sns = SnsCredential.create(
+        user_id: @user.id,
         provider: auth.provider,
         uid: auth.uid
         )
+      @user = User.find(@sns.user_id)
+    else
+      @user = User.find(sns.user_id)
     end
     @user
   end

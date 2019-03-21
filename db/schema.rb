364b79427f0b68decd0fee1f0dedbd6367a4ced9
ymtwithcarp <kkcymt@yahoo.co.jp>
2019-03-21 14:29:38 +0900

    upadte スキーマファイルの更新

--- db/schema.rb
+++ db/schema.rb
@@ -72,13 +72,6 @@ ActiveRecord::Schema.define(version: 2019_03_20_110116) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "trades", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "item_id"
-    t.string "status", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
     t.string "uid"
@@ -86,6 +79,13 @@ ActiveRecord::Schema.define(version: 2019_03_20_110116) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
+  create_table "trades", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "item_id"
+    t.string "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

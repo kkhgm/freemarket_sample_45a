46f513e3ac1fcc67bb95d6c545f5704f3ec9a7f4
cb50one <nakajimatakuyume@cb50one.local>
2019-03-21 17:58:06 +0900

    Fix ログイン必須
    ログインしていないとログイン必須ページに入れないようにしました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,8 @@ class ApplicationController < ActionController::Base
      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def sign_in_user
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 end

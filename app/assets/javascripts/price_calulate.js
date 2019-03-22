2e2d7246234ea407d79a01e5b6ad96e9ec2a2844
kkhgm <koki@higamimitsuterunoMacBook-Air.local>
2019-03-22 11:21:33 +0900

    Update js手直し

--- app/assets/javascripts/price_calulate.js
+++ app/assets/javascripts/price_calulate.js
@@ -1,16 +1,18 @@
-$(function() {
-  $(".item-contents__price_box_list_top_right_input").on('keyup', function(e){
-    e.preventDefault();
-    var count = $(this).val();
-    if(count >= 300){
-      var cut = count * 0.1
-	  var cutMoney = Math.floor(cut);
-	  $(".item-contents__price_box_list_sales").text("¥" + cutMoney);
-	  var salesMoney = count - cutMoney
-	  $(".item-contents__price_box_list_sales_under").text("¥" + salesMoney);
-    } else {
-      $(".item-contents__price_box_list_sales").text("-");
-	  $(".item-contents__price_box_list_sales_under").text("-");
-    }
+document.addEventListener('turbolinks:load', function() {
+  $(function() {
+    $(".item-contents__price_box_list_top_right_input").on('keyup', function(e){
+      e.preventDefault();
+      var count = $(this).val();
+      if(count >= 300){
+        var cut = count * 0.1
+  	  var cutMoney = Math.floor(cut);
+  	  $(".item-contents__price_box_list_sales").text("¥" + cutMoney);
+  	  var salesMoney = count - cutMoney
+  	  $(".item-contents__price_box_list_sales_under").text("¥" + salesMoney);
+      } else {
+        $(".item-contents__price_box_list_sales").text("-");
+  	  $(".item-contents__price_box_list_sales_under").text("-");
+      }
+    });
   });
 });
